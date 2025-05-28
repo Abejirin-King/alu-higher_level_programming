cd979b4fd4f6b059fdb155b6dee918f8623a0f2a
King Abejrin <k.abejirin@alustudent.com>
2025-05-28 04:59:35 -0700

    done

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -2,11 +2,9 @@
 
 const args = process.argv.slice(2);
 
-if (args[0] && args[1]) {
+if (args.length === 2) {
   console.log(`${args[0]} is ${args[1]}`);
-} else if (args[0]) {
+} else if (args.length === 1) {
   console.log(args[0]);
-} else {
-  console.log('No argument');
 }
 
